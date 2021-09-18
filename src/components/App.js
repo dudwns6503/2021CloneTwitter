7c63d5104569aa578dafedc655197143efd8c10b
Kimyeongjun <dudwns650395@gmail.com>
2021-09-18 23:53:35 +0900

    update: 로그인, 소셜 로그인 및 로그아웃 구현

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import AppRouter from "components/Router"
+import { authService } from "fbase";
+
+function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(user);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "initializing..."}
+      <footer>&copy; {new Date().getFullYear()} Clone Twitter</footer>
+    </>
+  );
+}
+
+export default App;
